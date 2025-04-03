1a04d67feb45d8fb8219236b22ba009ead47d6c1
IhabProjects <eureka.lucis@gmail.com>
2025-04-03 22:10:12 +0000

    add: basic comand line argument parser

---
+++ utils/oneLineArg.py
@@ -0,0 +1,10 @@
+import argparse
+
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description= "Fetch Github Activity")
+
+    parser.add_argument('--user', '-u', help = "Github username to fetch activity for", type = str, required = True)
+    parser.add_argument('--repo', '-r', help = "Github repository in format owner/repo", type = str, required = False)
+
+    return parser.parse_args()
