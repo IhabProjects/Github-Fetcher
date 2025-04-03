06eda0878f4ed31da4f15854e5eafd641450c043
IhabProjects <eureka.lucis@gmail.com>
2025-04-03 22:53:30 +0000

    Improve event formatter with better event type descriptions

--- utils/eventFormatter.py
+++ utils/eventFormatter.py
@@ -3,6 +3,17 @@ from datetime import datetime
 class EventFormatter:
     """Formats event data for display."""
 
+    EVENTS_TYPES = {
+        "PushEvent": "pushed to",
+        "IssuesEvent": "modified issue in",
+        "PullRequestEvent": "opened a pull request in",
+        "IssueCommentEvent": "commented on an issue in",
+        "WatchEvent": "starred",
+        "ForkEvent": "forked",
+    }
+
+
+
     @staticmethod
     def format_time(time_str):
         """Formats the current time."""
@@ -16,5 +27,17 @@ class EventFormatter:
         actor = event["actor"]["login"]
         repo = event["repo"]["name"]
         created_at = EventFormatter.format_time(event["created_at"])
+
+        action = EventFormatter.EVENTS_TYPES.get(event_type, "Interacted with")
+        if event_type == "PushEvent":
+            action = f"{action} {event['payload']['size']} commits"
+        elif event_type == "IssuesEvent":
+            action = f"{action} issue #{event['payload']['issue']['number']}"
+        elif event_type == "PullRequestEvent":
+            action = f"{action} pull request #{event['payload']['pull_request']['number']}"
+        elif event_type == "IssueCommentEvent":
+            action = f"{action} issue #{event['payload']['issue']['number']}"
+        elif event_type == "WatchEvent":
+            action = f"{action} {repo}"
 
         return f"{created_at} - {actor} performed {event_type} on {repo}"
