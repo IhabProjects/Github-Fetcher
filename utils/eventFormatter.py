30a98cbf7d12be1a3923ea1de4319bd736bea079
IhabProjects <eureka.lucis@gmail.com>
2025-04-03 23:36:23 +0000

    Improve event formatter with more event types

--- utils/eventFormatter.py
+++ utils/eventFormatter.py
@@ -3,20 +3,28 @@ from datetime import datetime
 class EventFormatter:
     """Formats event data for display."""
 
-    EVENTS_TYPES = {
+    EVENT_TYPES = {
         "PushEvent": "pushed to",
         "IssuesEvent": "modified issue in",
-        "PullRequestEvent": "opened a pull request in",
-        "IssueCommentEvent": "commented on an issue in",
+        "IssueCommentEvent": "commented on issue in",
+        "PullRequestEvent": "created PR in",
+        "PullRequestReviewEvent": "reviewed PR in",
+        "PullRequestReviewCommentEvent": "commented on PR in",
+        "CreateEvent": "created",
+        "DeleteEvent": "deleted",
         "WatchEvent": "starred",
         "ForkEvent": "forked",
+        "ReleaseEvent": "released in",
+        "CommitCommentEvent": "commented on commit in",
+        "PublicEvent": "made public",
+        "MemberEvent": "updated member in",
     }
 
 
 
     @staticmethod
     def format_time(time_str):
-        """Formats the current time."""
+        """Formats GitHub time string to human-readable format"""
         dt = datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%SZ")
         return dt.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -39,5 +47,22 @@ class EventFormatter:
             action = f"{action} issue #{event['payload']['issue']['number']}"
         elif event_type == "WatchEvent":
             action = f"{action} {repo}"
-
-        return f"{created_at} - {actor} performed {event_type} on {repo}"
+        elif event_type == "ForkEvent":
+            action = f"{action} {repo}"
+        elif event_type == "ReleaseEvent":
+            action = f"{action} {event['payload']['release']['tag_name']}"
+        elif event_type == "CommitCommentEvent":
+            action = f"{action} commit {event['payload']['comment']['commit_id']}"
+        elif event_type == "PublicEvent":
+            action = f"{action} {repo}"
+        elif event_type == "MemberEvent":
+            action = f"{action} {event['payload']['member']['login']}"
+        elif event_type == "DeleteEvent":
+            action = f"{action} {event['payload']['ref_type']} {event['payload']['ref']}"
+        elif event_type == "CreateEvent":
+            action = f"{action} {event['payload']['ref_type']} {event['payload']['ref']}"
+        elif event_type == "PullRequestReviewEvent":
+            action = f"{action} pull request #{event['payload']['pull_request']['number']}"
+        elif event_type == "PullRequestReviewCommentEvent":
+            action = f"{action} pull request #{event['payload']['pull_request']['number']}"
+        return f"{created_at} - {actor} {action} {repo}"
