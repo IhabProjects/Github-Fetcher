964084678c97301557686816d0f992a8609a3ef7
IhabProjects <eureka.lucis@gmail.com>
2025-04-03 23:17:16 +0000

    Add pagination options to argument parser

--- utils/oneLineArg.py
+++ utils/oneLineArg.py
@@ -8,4 +8,7 @@ def parse_args():
     parser.add_argument('--repo', '-r', help = "Github repository in format owner/repo", type = str, required = False)
     parser.add_argument('--token', '-t', help = "Github personal access token", type = str, required = False)
 
+    parser.add_argument('--page', '-p', help = "Page number for pagination", type = int, default = 1)
+    parser.add_argument('--count', '-c', help = "Number of events per page", type = int, default = 30)
+
     return parser.parse_args()
