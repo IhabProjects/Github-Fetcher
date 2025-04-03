0f42a15701dc0646802be5cc64d3be2ed718a075
IhabProjects <eureka.lucis@gmail.com>
2025-04-03 22:39:27 +0000

    Update: Add token support to argument parser

--- utils/oneLineArg.py
+++ utils/oneLineArg.py
@@ -6,5 +6,6 @@ def parse_args():
 
     parser.add_argument('--user', '-u', help = "Github username to fetch activity for", type = str, required = True)
     parser.add_argument('--repo', '-r', help = "Github repository in format owner/repo", type = str, required = False)
+    parser.add_argument('--token', '-t', help = "Github personal access token", type = str, required = False)
 
     return parser.parse_args()
