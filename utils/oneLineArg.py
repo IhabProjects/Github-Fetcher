2b7da7a9c570dd081fc2dcdbeae88530ced47320
IhabProjects <eureka.lucis@gmail.com>
2025-04-03 23:24:33 +0000

    Add option for JSON output format

--- utils/oneLineArg.py
+++ utils/oneLineArg.py
@@ -11,4 +11,7 @@ def parse_args():
     parser.add_argument('--page', '-p', help = "Page number for pagination", type = int, default = 1)
     parser.add_argument('--count', '-c', help = "Number of events per page", type = int, default = 30)
 
+
+    parser.add_argument('--json', '-j', action='store_true', help='Output raw JSON instead of formatted text')
+
     return parser.parse_args()
