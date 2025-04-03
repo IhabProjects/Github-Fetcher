ca0693c3b42c4c9cd659afc4df6c57d588848188
IhabProjects <eureka.lucis@gmail.com>
2025-04-03 22:37:51 +0000

    Create main fetcher script with basic user activity fetching

---
+++ github_fetcher.py
@@ -0,0 +1,26 @@
+
+
+import sys
+from utils.apiHandler import GitHubApi
+from utils.eventFormatter import EventFormatter
+from utils.oneLineArg import parse_args
+
+def main():
+    """Main entry point for the script."""
+    args = parse_args()
+
+    # Create API Handler
+    api = GitHubApi()
+
+    # Fetch events based on passed arguments
+
+    if args.user:
+        events = api.get_user_events(username = args.user)
+        for event in events:
+            print(EventFormatter.format_event(event))
+        else:
+            print("Error: --user is required.")
+            sys.exit(1)
+
+if __name__ == "__main__":
+    main()
